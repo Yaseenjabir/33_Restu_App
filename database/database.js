e4f3bc520041109c27a1463d85f529f1e985c20e
Ezgod な <103773402+Yaseenjabir@users.noreply.github.com>
2024-10-01 22:06:08 +0500

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,4 +1,4 @@
-const username = "antoniolima102938";
-const password = "Yaseen1234";
+const username = "yourUsername";
+const password = "yourPassword";
 
 export const dbref = `mongodb+srv://antoniolima102938:${password}@cluster0.pdqqsbp.mongodb.net/hotel?retryWrites=true&w=majority&appName=Cluster0`;
